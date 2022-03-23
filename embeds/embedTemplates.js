4a9822e94b75acca6deaa6f97288a245c83fabb2
Oscar Capraro <oscar@mcapraro.com>
2022-03-23 11:20:49 -0700

    feat: light mode shift

--- embeds/embedTemplates.js
+++ embeds/embedTemplates.js
@@ -2,6 +2,20 @@ export const basicEmbed = (title, content, width, style="") => {
   let box = `
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="${width}" height="200" viewBox="0 0 ${width} 200">
       <style>
+        @media (prefers-color-scheme: dark) {
+          :root {
+            --background-color:#151515;
+            --title-color:#fff;
+            --border-color:#fff;
+          }
+        }
+        @media (prefers-color-scheme: light) {
+          :root {
+            --background-color:#fff;
+            --title-color:#000;
+            --border-color:#000;
+          }
+        }
         text {
           font-family: sans-serif;
           font-weight: bold;
@@ -10,14 +24,19 @@ export const basicEmbed = (title, content, width, style="") => {
           font-size: 14;
           fill: #9E9E9E;
         }
+        #title {
+          fill:var(--title-color);
+          font-size: 19px;
+        }
+        #bg {
+          fill:var(--background-color);
+          stroke:var(--border-color);
+        } 
         ${style}
       </style>
-      <rect width="100%" height="100%" fill="#151515" rx="4.5" style="stroke-width:2;stroke:#fff" x="0" y="0" />
+      <rect width="100%" height="100%" id="bg" rx="4.5" x="0" y="0" />
       <g transform="translate(25, 35)">
-        <text x="0" y="0" style="
-          fill:white;
-          font-size: 19;
-        ">${title}</text>
+        <text x="0" y="0" id="title">${title}</text>
         ${content}
       </g>
     </svg>
